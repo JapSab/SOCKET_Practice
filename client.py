be4cabbbb396b179ed9a488bacc5e3e3beb8b21f
JapSab <sabaj127001@gmail.com>
2023-05-30 13:21:24 +0400

    modified the byte size

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import socket
 
 
-HEADER = 64
+HEADER = 4
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
 
-# Make a variable call it Server and use your ipv4 address for client 
-
+# Make a string type variable call it SERVER and use your ipv4 address for client 
+SERVER = '127.0.1.1'
 ADDR = (SERVER, PORT) 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
